e803778fc522ddd0bb35c8edf8926b483b1d88e8
cosmologychen <a1219511624@outlook.com>
2026-02-06 15:16:32 +0800

    删除没用文件

--- module_load_t7920.sh
+++
@@ -1,3 +0,0 @@
-# source /opt/intel/oneapi/setvars.sh
-
-# run executable by ./a.out and set number of images by FOR_COARRAY_NUM_IMAGES
--- reps-master/INCLUDE/write_output.h
+++
@@ -1,5 +0,0 @@
-/**   WRITE OUTPUT ************************************************************/
-extern void write_output(int knum, double *k,
-      double **Delta_b,double **Delta_c,double **Delta_n,double **Delta_m,
-      double **growth_b,double **growth_c,double **growth_n,double **growth_m);
-/******************************************************************************/
